46a53468794bd4abb24366a6ecd74bfe61a8c655
Josiah bwana <51917388+joshbwana@users.noreply.github.com>
2019-09-09 23:00:50 +0100

    Update FILE BULLDOZER.py

--- FILE BULLDOZER.py
+++ FILE BULLDOZER.py
@@ -1,8 +1,5 @@
 import os
 import shutil
-#MAIN = r'\\wsetbn-semantex\Orange_Pride\Logs\REL19_09_Execution_Videos\v-jobwan'
-#MAIN = r'C:\Users\\Bwana\Desktop\kombo'
-#MAIN=r'\\wsetbn-semantex\Orange_Pride\SEManTEX\Tools'
 print("="*60)
 print("THIS PROGRAM IS COURTESY OF JOSIAH BWANA\nITS PURPOSE IS TO EASE MOVING FILES IN BULK")
 print("="*60)
@@ -10,12 +7,9 @@ print("\n")
 print("ENTER PATH TO MOVE FILES TO")
 mov_to=input()
 MAIN=mov_to
-#MAIN = r'C:\Users\\Bwana\Desktop\New'
-#DIRS = r'C:\Users\Bwana\Desktop\New folder'
 #DIRS = r'C:\Users\Bwana\Desktop\.netv4'
 print("ENTER PATH TO MOVE FILES FROM")
 DIRS = input()
-#DIRS = r'\\wsetbn-semantex\Orange_Pride\Logs\REL19_09_Execution_Videos\v-jobwan'
 for root, subdirs, files in os.walk(DIRS):
     print('root', root)
     print('root', subdirs)
